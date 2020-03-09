51b5f465e10baddd76b0110ffdd43651ddd88362
202637249 <202637249@qq.com>
2020-03-09 16:42:32 +0800

    Initial commit

---
+++ src/test/java/com/springboot/lzq/LzqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.lzq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LzqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
